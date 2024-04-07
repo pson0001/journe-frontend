f387fedf9e95fc909316c7dbf21500d5c596ec69
Ping Song <pson0001@hotmail.com>
2024-04-07 20:41:18 +1000

    sort tasks

--- src/components/pages/tasks-container/task-list/TaskList.jsx
+++ src/components/pages/tasks-container/task-list/TaskList.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import c from "./task-list.module.scss";
 import TaskItem from "../task-item/TaskItem";
 const TaskList = ({ tasks, pot }) => {
+  const sortedTasks = tasks.sort((task1, task2) => {
+    return task1.task_start_time.localeCompare(task2.task_start_time);
+  });
+
   return (
     <div className={c.listContainer}>
-      {tasks?.map((task, i) => {
+      {sortedTasks?.map((task, i) => {
         if (task?.task_pot_id === pot?.pot_id) {
           return <TaskItem key={i} task={task} potId={pot?.pot_id} />;
         }
